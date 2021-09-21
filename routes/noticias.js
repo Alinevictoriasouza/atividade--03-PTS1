565fedf6b4f5b2f308dde2fbdc4eea6e2202983d
Aline Souza <86006464+Alinevictoriasouza@users.noreply.github.com>
2021-09-21 21:33:07 +0000

    Mudei os nomes nas rotas de acordo com o que foi pedido

--- routes/noticias.js
+++ routes/noticias.js
@@ -6,19 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('noticias/index', { title: 'Express' });
 });
 
-router.get('/noticia1', function(req, res, next) {
+router.get('/01', function(req, res, next) {
   res.render('noticias/noticia1', { title: 'Express' });
 });
 
-router.get('/noticia2', function(req, res, next) {
+router.get('/02', function(req, res, next) {
   res.render('noticias/noticia2', { title: 'Express' });
 });
 
-router.get('/noticia3', function(req, res, next) {
+router.get('/03', function(req, res, next) {
   res.render('noticias/noticia3', { title: 'Express' });
 });
 
-router.get('/noticia4', function(req, res, next) {
+router.get('/04', function(req, res, next) {
   res.render('noticias/noticia4', { title: 'Express' });
 });
 
