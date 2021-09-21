ec4812a65fde77f965e99abea18229b3bf887ee5
Aline Souza <86006464+Alinevictoriasouza@users.noreply.github.com>
2021-09-21 20:57:57 +0000

    rotas para cada noticia foi criada

--- routes/noticias.js
+++ routes/noticias.js
@@ -6,6 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('noticias/index', { title: 'Express' });
 });
 
+router.get('/noticia1', function(req, res, next) {
+  res.render('noticias/noticia1', { title: 'Express' });
+});
+
+router.get('/noticia2', function(req, res, next) {
+  res.render('noticias/noticia2', { title: 'Express' });
+});
+
+router.get('/noticia3', function(req, res, next) {
+  res.render('noticias/noticia3', { title: 'Express' });
+});
+
+router.get('/noticia4', function(req, res, next) {
+  res.render('noticias/noticia4', { title: 'Express' });
+});
 
 
 module.exports = router;
